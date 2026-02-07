b4bf539fa4f5a97a8d2683b6189347018d55d5ec
DonTrevisiol <valentin.trevi@gmail.com>
2026-02-07 18:13:18 -0400

    Fix GoatCounter server online

--- assets/js/goatcounter.js
+++ assets/js/goatcounter.js
@@ -1,28 +1,15 @@
-function updateVisitCount() {
-    console.log("Comprobando contador...");
-
-    const el = document.querySelector("#gc-visits");
-
-    if (!el) {
-        console.log("No existe #gc-visits");
-        return;
-    }
-
-    if (window.goatcounter && goatcounter.visit_count) {
-        const count = goatcounter.visit_count.count;
-        console.log("Visitas recibidas:", count);
-
-        el.textContent = count ?? "0";
-    } else {
-        console.log("GoatCounter aún no está listo...");
-    }
-}
-
-// Esperar a que cargue la página
-window.addEventListener("load", () => {
-    // Intentar varias veces por carga lenta
-    setTimeout(updateVisitCount, 1000);
-    setTimeout(updateVisitCount, 2000);
-    setTimeout(updateVisitCount, 3000);
-    setTimeout(updateVisitCount, 5000);
-});
+function loadVisits() {
+	console.log("window.goatcounter: ", window.goatcounter);
+	console.log("goatcounter.visit_count: ", goatcounter.visit_count)
+	if (window.goatcounter && goatcounter.visit_count) {
+		goatcounter.visit_count({
+			append: "#gc-visits",
+			type: "html",
+			no_branding: true
+			});
+		} else {
+			setTimeout(loadVisits, 3000)
+			}
+	}
+	
+	loadVisits();
