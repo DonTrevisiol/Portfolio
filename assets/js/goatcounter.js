f446b94d716da4e857ca6d27c786fd31188a9d9b
DonTrevisiol <valentin.trevi@gmail.com>
2026-02-07 16:32:53 -0400

    New file goatcounter.js add and script update

---
+++ assets/js/goatcounter.js
@@ -0,0 +1,29 @@
+function debugGoatCounter() {
+	console.log("DEBUG: comprobando GoatCounter...");
+	}
+	
+const el = document.querySelector("#gc-visits");
+console.log("Elemento contador: ", el);
+
+if (!el) {
+	console.log("No existe #gc-visits");
+	return;
+	}
+	
+console.log("window.goatcounter =", window.goatcounter);
+
+if (window.goatcounter && goatcounter.visit_count) {
+	console.log("visit_count: ", goatcounter.visit_count);
+	
+	const count = goatcounter.visit_count.count;
+	console.log("Número recibido: ", count);
+	
+	el.textContent = count ?? "0";
+	} else {
+		console.log("GoatCounter aun no esta listo.");
+		}
+	}
+	
+	setTimeout(debugGoatCounter, 1000);
+	setTimeout(debugGoatCounter, 2000);
+	setTimeout(debugGoatCounter, 3000);
