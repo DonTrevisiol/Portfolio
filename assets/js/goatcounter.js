787342322ad34d40a6bac5bfb0cdaa7ccfb6b88d
DonTrevisiol <valentin.trevi@gmail.com>
2026-02-07 18:21:22 -0400

    Fix I hope the last

--- assets/js/goatcounter.js
+++ assets/js/goatcounter.js
@@ -1,15 +1,19 @@
 function loadVisits() {
-	console.log("window.goatcounter: ", window.goatcounter);
-	console.log("goatcounter.visit_count: ", goatcounter.visit_count)
-	if (window.goatcounter && goatcounter.visit_count) {
-		goatcounter.visit_count({
-			append: "#gc-visits",
-			type: "html",
-			no_branding: true
-			});
-		} else {
-			setTimeout(loadVisits, 3000)
-			}
-	}
-	
-	loadVisits();
+  console.log("window.goatcounter:", window.goatcounter);
+
+  if (window.goatcounter && window.goatcounter.visit_count) {
+    console.log("GoatCounter listo");
+
+    window.goatcounter.visit_count({
+      append: "#gc-visits",
+      type: "html",
+      no_branding: true
+    });
+
+  } else {
+    console.log("GoatCounter aún no cargó, reintentando...");
+    setTimeout(loadVisits, 1000);
+  }
+}
+
+loadVisits();
